8c73ed0b155e8695c26b45c5a9383fa98ac5824d
Jagdish Parihar <jatin6972@gmail.com>
2023-06-08 23:48:45 +0530

    types for the variables

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -1,22 +1,22 @@
 import { Request, Response } from 'express';
-import { User } from '../model/user';
+import { IUser, User } from '../model/user';
 
 export const createUser = async (req: Request, res: Response) => {
   const { username, password, age } = req.body;
 
   // check if user already exists
   try {
-    const user = await User.findOne({ username });
+    const user: IUser | null = await User.findOne({ username });
 
     if (user) {
-      throw new Error('User already exists');
+      throw new Error('Username already exists');
     }
   } catch (error: any) {
     return res.status(400).json({ ok: false, error: error.message });
   }
 
   try {
-    const user = await User.create({ username, password, age });
+    const user: IUser = await User.create({ username, password, age });
     return res.status(201).json({ ok: true, data: user });
   } catch (error) {
     return res.status(400).json({ ok: false, error: error });
@@ -27,7 +27,7 @@ export const editUser = async (req: Request, res: Response) => {
   const { username, password, age, id } = req.body;
 
   try {
-    const user = await User.findOne({ _id: id });
+    const user: IUser | null = await User.findOne({ _id: id });
 
     if (!user) {
       throw new Error('User not exists');
@@ -37,7 +37,11 @@ export const editUser = async (req: Request, res: Response) => {
   }
 
   try {
-    const user = await User.updateOne({ _id: id }, { username, password, age });
+    const user: IUser | null = await User.findOneAndUpdate(
+      { _id: id },
+      { username, password, age },
+      { new: true },
+    );
     return res.status(201).json({ ok: true, data: user });
   } catch (error) {
     return res.status(400).json({ ok: false, error: error });
