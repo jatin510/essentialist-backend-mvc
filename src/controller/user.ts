f007b650d33b0479aa71ae07d79d0c45b26a1d7e
Jagdish Parihar <jatin6972@gmail.com>
2023-06-08 05:41:54 +0530

    added logic in controller

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -1,9 +1,23 @@
 import { Request, Response } from 'express';
+import { User } from '../model/user';
 
-export const createUser = (req: Request, res: Response) => {
-  return;
+export const createUser = async (req: Request, res: Response) => {
+  const { username, password, age } = req.body;
+  try {
+    const user = await User.create({ username, password, age });
+    return res.status(201).json({ ok: true, data: user });
+  } catch (error) {
+    return res.status(400).json({ ok: false, error: error });
+  }
 };
 
-export const editUser = (req: Request, res: Response) => {
-  return;
+export const editUser = async (req: Request, res: Response) => {
+  const { username, password, age, id } = req.body;
+
+  try {
+    const user = await User.updateOne({ _id: id }, { username, password, age });
+    return res.status(201).json({ ok: true, data: user });
+  } catch (error) {
+    return res.status(400).json({ ok: false, error: error });
+  }
 };
