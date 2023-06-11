4ce696e68ccf928ec567916487414bbb7a3e73ab
Jagdish Parihar <jatin6972@gmail.com>
2023-06-11 18:07:05 +0530

    using bootstrap.js file to start the server

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,14 +1,25 @@
-// import app from './app';
-// import { config } from './config';
-// import { MongoDB } from './mongoDB';
+import app from './app';
+import { config } from './config';
+import { MongoDB } from './mongoDB';
 
-// (async () => {
-//   // start monogdb
-//   const mongoDB = new MongoDB(config);
-//   await mongoDB.start();
+(async () => {
+  // start monogdb
+  const mongoDB = new MongoDB(config);
+  try {
+    await mongoDB.start();
+    console.log('mongoose connected successfully');
+  } catch (error) {
+    console.log('error connecting to mongodb', error);
+    return;
+  }
 
-//   // start Server
-//   await app.listen(config.port, () => {
-//     console.log('server running on port', config.port);
-//   });
-// })();
+  // start Server
+  try {
+    await app.listen(config.port, () => {
+      console.log('server running on port: ', config.port);
+    });
+  } catch (error) {
+    console.log('error connecting to the server ', error);
+    return;
+  }
+})();
